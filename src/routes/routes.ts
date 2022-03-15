431d608698ce5a7502af38e9343d05da2343c524
taintpro98 <taideptrai02@gmail.com>
2022-03-15 18:08:12 +0700

    finish upload server

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import multer from 'multer';
 
 import BreedingQueueController from '../controllers/BreedingQueueController';
+import { upload } from 'constants/images';
 
 const BreedingQueueControllerInstance = new BreedingQueueController();
 export function RegisterRoutes(app: express.Router){
     app.get("/", BreedingQueueControllerInstance.test.bind(BreedingQueueControllerInstance));
-    
+    app.post("/upload", upload.single('neko'), (req, res, next) => {
+        console.log(req.file, req.body)
+    });
 }
